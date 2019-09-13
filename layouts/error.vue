777f9c329ddb7610faccf364fd9e00bb5fa9d30f
Ryo Nakae <ryo.n.1129@gmail.com>
2019-09-13 19:21:48 +0900

    error page title

--- layouts/error.vue
+++ layouts/error.vue
@@ -9,10 +9,27 @@
 <script lang="ts">
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
 import { NuxtError } from '@nuxt/types/app'
+import * as Config from 'config'
 
 @Component
 export default class extends Vue {
+  // props
   @Prop({ type: Object, required: true })
   readonly error!: NuxtError
+
+  // head
+  head(): Config.Head {
+    return {
+      titleTemplate: undefined,
+      meta: [
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: process.env.SITE_TITLE,
+          template: '%s'
+        }
+      ]
+    }
+  }
 }
 </script>
