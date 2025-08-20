f9c38049df1487abcde45422042f64991dbaffb5
Ryo Nakae <ryo.n.1129@gmail.com>
2025-08-20 17:20:27 +0900

    feat: useHtml関数にカスタムコンポーネントのサポートを追加
    - HTML要素の属性型マッピングを定義し、カスタムコンポーネントを受け取れるように変更
    - aタグの処理を改善し、相対パスへの変換を実装

--- app/components/HtmlRenderer.vue
+++ app/components/HtmlRenderer.vue
@@ -3,9 +3,27 @@
 </template>
 
 <script setup lang="ts">
+import { NuxtLink } from '#components'
+
 const props = defineProps<{
   html: string
 }>()
 
-const { renderedContent } = useHtml(props.html)
+const { renderedContent } = useHtml(props.html, {
+  a: (props) => {
+    let linkTo = props.href
+
+    // サイトのURLで始まる場合は相対パスに変換
+    if (typeof linkTo === 'string' && linkTo.startsWith(siteInfo.url)) {
+      linkTo = linkTo.replace(siteInfo.url, '')
+    }
+
+    return h(
+      NuxtLink,
+      { to: linkTo, target: props.target, rel: props.rel },
+      // 子要素を関数として渡す
+      () => props.children,
+    )
+  },
+})
 </script>
--- app/composables/useHtml.ts
+++ app/composables/useHtml.ts
@@ -1,7 +1,182 @@
 import { parseFragment, type DefaultTreeAdapterTypes } from 'parse5'
-import { computed, h, type VNode, Fragment } from 'vue'
+import {
+  computed,
+  h,
+  type VNode,
+  Fragment,
+  type Component,
+  type HTMLAttributes,
+  type AnchorHTMLAttributes,
+  type AreaHTMLAttributes,
+  type AudioHTMLAttributes,
+  type BaseHTMLAttributes,
+  type BlockquoteHTMLAttributes,
+  type ButtonHTMLAttributes,
+  type CanvasHTMLAttributes,
+  type ColHTMLAttributes,
+  type ColgroupHTMLAttributes,
+  type DataHTMLAttributes,
+  type DelHTMLAttributes,
+  type DetailsHTMLAttributes,
+  type DialogHTMLAttributes,
+  type EmbedHTMLAttributes,
+  type FieldsetHTMLAttributes,
+  type FormHTMLAttributes,
+  type HtmlHTMLAttributes,
+  type IframeHTMLAttributes,
+  type ImgHTMLAttributes,
+  type InputHTMLAttributes,
+  type InsHTMLAttributes,
+  type KeygenHTMLAttributes,
+  type LabelHTMLAttributes,
+  type LiHTMLAttributes,
+  type LinkHTMLAttributes,
+  type MapHTMLAttributes,
+  type MenuHTMLAttributes,
+  type MediaHTMLAttributes,
+  type MetaHTMLAttributes,
+  type MeterHTMLAttributes,
+  type ObjectHTMLAttributes,
+  type OlHTMLAttributes,
+  type OptgroupHTMLAttributes,
+  type OptionHTMLAttributes,
+  type OutputHTMLAttributes,
+  type ParamHTMLAttributes,
+  type ProgressHTMLAttributes,
+  type QuoteHTMLAttributes,
+  type ScriptHTMLAttributes,
+  type SelectHTMLAttributes,
+  type SourceHTMLAttributes,
+  type StyleHTMLAttributes,
+  type TableHTMLAttributes,
+  type TdHTMLAttributes,
+  type TextareaHTMLAttributes,
+  type ThHTMLAttributes,
+  type TimeHTMLAttributes,
+  type TrackHTMLAttributes,
+  type VideoHTMLAttributes,
+  type WebViewHTMLAttributes,
+} from 'vue'
 
-export function useHtml(html: string) {
+// HTML要素の属性型マッピング
+type ElementAttributesMap = {
+  a: AnchorHTMLAttributes
+  area: AreaHTMLAttributes
+  audio: AudioHTMLAttributes
+  base: BaseHTMLAttributes
+  blockquote: BlockquoteHTMLAttributes
+  button: ButtonHTMLAttributes
+  canvas: CanvasHTMLAttributes
+  col: ColHTMLAttributes
+  colgroup: ColgroupHTMLAttributes
+  data: DataHTMLAttributes
+  del: DelHTMLAttributes
+  details: DetailsHTMLAttributes
+  dialog: DialogHTMLAttributes
+  embed: EmbedHTMLAttributes
+  fieldset: FieldsetHTMLAttributes
+  form: FormHTMLAttributes
+  html: HtmlHTMLAttributes
+  iframe: IframeHTMLAttributes
+  img: ImgHTMLAttributes
+  input: InputHTMLAttributes
+  ins: InsHTMLAttributes
+  keygen: KeygenHTMLAttributes
+  label: LabelHTMLAttributes
+  li: LiHTMLAttributes
+  link: LinkHTMLAttributes
+  map: MapHTMLAttributes
+  menu: MenuHTMLAttributes
+  meta: MetaHTMLAttributes
+  meter: MeterHTMLAttributes
+  object: ObjectHTMLAttributes
+  ol: OlHTMLAttributes
+  optgroup: OptgroupHTMLAttributes
+  option: OptionHTMLAttributes
+  output: OutputHTMLAttributes
+  param: ParamHTMLAttributes
+  progress: ProgressHTMLAttributes
+  q: QuoteHTMLAttributes
+  script: ScriptHTMLAttributes
+  select: SelectHTMLAttributes
+  source: SourceHTMLAttributes
+  style: StyleHTMLAttributes
+  table: TableHTMLAttributes
+  td: TdHTMLAttributes
+  textarea: TextareaHTMLAttributes
+  th: ThHTMLAttributes
+  time: TimeHTMLAttributes
+  track: TrackHTMLAttributes
+  video: VideoHTMLAttributes
+  webview: WebViewHTMLAttributes
+  // 一般的なHTML要素（特定の属性型がないもの）
+  div: HTMLAttributes
+  span: HTMLAttributes
+  p: HTMLAttributes
+  h1: HTMLAttributes
+  h2: HTMLAttributes
+  h3: HTMLAttributes
+  h4: HTMLAttributes
+  h5: HTMLAttributes
+  h6: HTMLAttributes
+  ul: HTMLAttributes
+  thead: HTMLAttributes
+  tbody: HTMLAttributes
+  tfoot: HTMLAttributes
+  tr: HTMLAttributes
+  pre: HTMLAttributes
+  code: HTMLAttributes
+  strong: HTMLAttributes
+  em: HTMLAttributes
+  br: HTMLAttributes
+  hr: HTMLAttributes
+  section: HTMLAttributes
+  article: HTMLAttributes
+  header: HTMLAttributes
+  footer: HTMLAttributes
+  nav: HTMLAttributes
+  aside: HTMLAttributes
+  main: HTMLAttributes
+  figure: HTMLAttributes
+  figcaption: HTMLAttributes
+  mark: HTMLAttributes
+  small: HTMLAttributes
+  sub: HTMLAttributes
+  sup: HTMLAttributes
+  i: HTMLAttributes
+  b: HTMLAttributes
+  u: HTMLAttributes
+  s: HTMLAttributes
+  cite: HTMLAttributes
+  abbr: HTMLAttributes
+  dfn: HTMLAttributes
+  kbd: HTMLAttributes
+  samp: HTMLAttributes
+  var: HTMLAttributes
+  wbr: HTMLAttributes
+  dd: HTMLAttributes
+  dl: HTMLAttributes
+  dt: HTMLAttributes
+}
+
+// カスタムコンポーネントの props の型定義（ジェネリック型を使用）
+export interface CustomComponentProps {
+  children?: VNode[]
+}
+
+// カスタムコンポーネントの型定義（特定のHTML要素の属性型を受け取る）
+export type CustomComponent<T extends keyof ElementAttributesMap = keyof ElementAttributesMap>
+  = Component
+    | ((props: ElementAttributesMap[T] & CustomComponentProps) => VNode)
+
+// カスタムコンポーネントのマップの型定義（型推論を向上）
+export type CustomComponents = {
+  [K in keyof ElementAttributesMap]?: (props: ElementAttributesMap[K] & CustomComponentProps) => VNode
+} & {
+  [tagName: string]: (props: HTMLAttributes & CustomComponentProps) => VNode
+}
+
+export function useHtml(html: string, customComponents?: CustomComponents) {
   const documentFragment = parseFragment(html)
 
   const isElement = (node: DefaultTreeAdapterTypes.ChildNode): node is DefaultTreeAdapterTypes.Element => {
@@ -23,7 +198,7 @@ export function useHtml(html: string) {
         return null
       }
       // テキストノードもVNodeとして返す
-      return h('span', { style: { display: 'contents' } }, text)
+      return h('span', text)
     }
 
     if (!isElement(node)) {
@@ -50,6 +225,23 @@ export function useHtml(html: string) {
           children.push(renderedChild)
         }
       })
+    }
+
+    // カスタムコンポーネントが定義されている場合はそれを使用
+    const customComponent = customComponents?.[tagName]
+    if (customComponent) {
+      const props: CustomComponentProps = {
+        ...attrs,
+        children: children.length > 0 ? children : undefined,
+      }
+
+      // カスタムコンポーネントが関数の場合は実行、そうでなければh()で処理
+      if (typeof customComponent === 'function' && !('render' in customComponent)) {
+        return (customComponent as (props: CustomComponentProps) => VNode)(props)
+      }
+      else {
+        return h(customComponent as Component, attrs, children)
+      }
     }
 
     return h(tagName, attrs, children)
